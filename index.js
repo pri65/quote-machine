d22acdbb157a4f80bad771ee829b4f28c3973751
pri65 <priyankasoniii65@gmail.com>
2022-11-01 19:05:26 +0530

    new

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ function App() {
     const [quotes,setQuotes] = React.useState([]);
     const [randomQuotes,setRandomQuotes] = React.useState([]);
 
+     React.useEffect(() => {
+       async function fetchData() {
+        const response = await fetch("https://type.fit/api/quotes")
+        const data = await response.json();
+       }
+     },[])
+
     return (
         <div>Hello World</div>
     );
